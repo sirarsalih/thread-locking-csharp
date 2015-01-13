69fa2ed43435b9b06f2b88ba54135580efd48cdf
sirar87 <sirars@gmail.com>
2015-01-13 22:47:30 +0100

    Initial commit.

---
+++ Thread.Locking/ObjectStack.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Thread.Locking
+{
+    enum ExitCode
+    {
+        Success = 0,
+        Error = 1
+    }
+
+    public class ObjectStack
+    {
+        private readonly Stack<Object> _objects = new Stack<object>();
+        private readonly Object _lockObject = new Object();
+        private const int NumOfPopIterations = 1000;
+
+        public ObjectStack(IEnumerable<object> objects)
+        {
+            foreach (var anObject in objects) {
+                Push(anObject);
+            }
+        }
+
+        public void Push(object anObject)
+        {
+            _objects.Push(anObject);
+        }
+
+        public void Pop()
+        {
+            _objects.Pop();
+        }
+
+        public void ThreadSafeMultiPop()
+        {
+            for (var i = 0; i < NumOfPopIterations; i++) {
+                lock (_lockObject) {
+                    try {
+                        Pop();
+                    }
+                    //Because of lock, the stack will be emptied safely and no exception is ever caught
+                    catch (InvalidOperationException) {
+                        Environment.Exit((int)ExitCode.Error);
+                    }
+                    if (_objects.Count == 0) {
+                        Environment.Exit((int)ExitCode.Success);
+                    }
+                }
+            }
+        }
+
+        public void ThreadUnsafeMultiPop()
+        {
+            for (var i = 0; i < NumOfPopIterations; i++) {
+                    try {
+                        Pop();
+                    }
+                    //Because there is no lock, an exception is caught when popping an already empty stack
+                    catch (InvalidOperationException) {
+                        Environment.Exit((int)ExitCode.Error);
+                    }
+                    if (_objects.Count == 0) {
+                        Environment.Exit((int)ExitCode.Success);
+                    }
+                }
+        }
+    }
+}
---
+++ Thread.Locking/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Thread.Locking
+{
+    public class Program
+    {
+        private const int NumOfObjects = 100;
+        private const int NumOfThreads = 10000;
+
+        public static void Main(string[] args)
+        {
+            var objects = new List<Object>();
+            for (var i = 0; i < NumOfObjects; i++) {
+                objects.Add(new object());
+            }
+            var objectStack = new ObjectStack(objects);
+            Parallel.For(0, NumOfThreads, x => objectStack.ThreadSafeMultiPop());
+        }
+    }
+}
